0eed7feeb1c95550de882ae7969e8235291d2038
ayaqabbani <ayaqabbani998@gmail.com>
2021-03-15 22:41:27 +0200

    added a button

--- js/app.js
+++ js/app.js
@@ -28,7 +28,6 @@ const centerImage=document.getElementById('centerImage');
 const rightImage=document.getElementById('rightImage');
 let counter = 0;
 
-// shop class
 function Shop(name,imgExt){
     this.name=name;
     this.views=0;
@@ -41,22 +40,30 @@ let shopArray=[];
 function randomNumber(min,max){
     return Math.floor(Math.random() * (max - min +1)) + min
 }
-
+let viewsList=[];
 function constructProductImage(image){
     let index = randomNumber(0,shopArray.length-1);
     let selectedShop = shopArray[index];
-    selectedShop.views++;
+    shopArray[index].views++;
     image.src = selectedShop.path;
-    image.title = selectedShop.products;
+    image.title = selectedShop.name;
     image.alt = selectedShop.name;
 }
 
 function doRender() {
-    for (let i=0;i<10;i++){
+    for (let i=0;i<products.length;i++){
        let productName = products[i];
-       let shop = new Shop(productName,'jpg');
+       let path;
+       if ( products[i] === 'sweep'){
+        path = 'png'
+       }else if (products[i] === 'usb'){
+            path = 'gif'
+        } 
+        else {
+            path= 'jpg'
+        }
+       let shop = new Shop(productName,path);
        shopArray[i] = shop;
-
     }
 
     render();
@@ -68,38 +75,62 @@ function showResult() {
     for (let i=0;i<shopArray.length;i++){
        let li= document.createElement('li');
        let shop=shopArray[i];
-       li.textContent='products = ' + shop.name +  ' ,views = ' + shop.views + ' ,votes = ' + shop.votes;
+       li.textContent = shop.name + ' views: ' + shop.views + ' : votes = '  + shop.votes;
        list.appendChild(li);
        }
     resultSection.appendChild(list);
 }
-
+let votingSession = 25
 function render() {
-    if (counter == 25){
-        showResult();
-        return;
-    }
     constructProductImage(leftImage);
     constructProductImage(centerImage);
     constructProductImage(rightImage);
     counter++;
+   
 }
 
-  
+
+
+
 function clickHandler(event){
+    votingSession--;
+    console.log(votingSession);
+
     if (event.target.id == 'leftImage' 
     || event.target.id == 'centerImage'
     || event.target.id == 'rightImage'){
-        for(let i=0;i<shopArray.length;i++){
-            if (shopArray[i].name == event.target.title){
-                shopArray[i].votes++;
-                console.table(shopArray[i]);
-            }
-        }
-        render();
+            for(let i=0;i<shopArray.length;i++){
+                if (shopArray[i].name == event.target.title){
+                    shopArray[i].votes++;
+                    // console.log(shopArray[i].votes);
+            } 
+
     }
+}
+if (votingSession===0){
+    alert('ilove you');
+    createResultButton();
+    imageSection.removeEventListener('click',clickHandler);
+}
+render();
+
+
 }
 
 imageSection.addEventListener('click',clickHandler);
+function createResultButton(){
+    alert('ilove you');
+    let button = document.createElement('button');
+    button.textContent= "show results";
+    button.addEventListener ("click",showResult)
+  resultSection.appendChild(button);
+ 
+}
+
+function resultsButtonHandler(){
+    showResult();
+}
+
 
 doRender();
+render();
